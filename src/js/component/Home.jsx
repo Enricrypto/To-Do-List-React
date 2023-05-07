67819df3afe4aff0295f2b562a3967f06a5fd057
Enricrypto <eibarraf@gmail.com>
2023-05-07 19:17:26 +0000

    fix: delete todo function

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -14,22 +14,15 @@ const Home = () => {
 		setTodoList(todoList.filter((elem, currentIndex) => index != currentIndex))
 	}
 
-	const todo = (item) => {
-		setTodoList(todoList.filter((index) => {
-		return index !== item})); 
-	}
-
 	return (
 		<div className="container to-do-list">
 			<h1 className="text-center" >To-do-List</h1>
-			<Task addTask = {addTask}/>
+			<Task addTask = {addTask} />
 			{todoList.length ? 
-			<div className="card p-5 mt-2 d-flex justify-content-center">
 				<Todo todoList={todoList} deleteTask = {deleteTask} />
-			</div>
 			:
 			<div className="d-flex flex-column align-items-center">"No tasks, add a task"</div>}
-			</div>
+		</div>
 	);
 };
 
--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -3,15 +3,15 @@ import React, { useState } from 'react';
 const Todo = ({ todoList, deleteTask }) => {
 
     return (
-        <div>
-            {todoList.map((elem, index) => <div key={index} className="card w-50 mt-2 d-flex justify-content-center">
-                <div className="card-body d-flex-column">
+        <>
+            {todoList.map((elem, index) => <div key={index} className="card d-flex flex-column align-items-center mt-2">
                     <h5 className="card-title">{elem}</h5>
-                    <button className="btn btn-primary pl-1">Done</button>
-                    <button className="btn btn-danger" onClick={() => deleteTask(index)}>Delete</button>
-                </div>
+                    <div className="d-flex flex-row">
+                    <button className="btn btn-primary m-3">Done</button>
+                    <button className="btn btn-danger m-3" onClick={() => deleteTask(index)}>Delete</button>
+                    </div>
             </div>)}
-        </div>
+        </>
     )
 };
 
